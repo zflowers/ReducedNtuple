992ba4e28695cbf461521790d9de022af2057acb
Zachary Flowers <zacharyflowers96@gmail.com>
2019-10-17 19:07:44 -0500

    Fixed File Issue for Plotting

--- macros/run_Eff_Nano_Hist.cc
+++ macros/run_Eff_Nano_Hist.cc
@@ -7,30 +7,27 @@ void run_Eff_Nano_Hist(string outFile = "output_test.root"){
  gROOT->ProcessLine(".x prod2017MC_reducedNANO_MET.C++");
  gROOT->ProcessLine(".x prod2018MC_reducedNANO_MET.C++");
  gROOT->ProcessLine(".L Plotter_Eff_Nano.C++"); 
- 
+  
  //2016
- gROOT->ProcessLine("TChain chain_2016(\"Events\")");
- gROOT->ProcessLine("chain_2016.Add(\"~/../janguian/ReducedNtuple/output/prod2016MC_reducedNANO_MET.root\")");
- //gROOT->ProcessLine("chain_2016.Add(\"../samples/prod2016MC_reducedNANO_MET.root\")");
- gROOT->ProcessLine("prod2016MC_reducedNANO_MET MET_2016(chain_2016.GetTree())");
+ gROOT->ProcessLine("TChain* chain_2016 = new TChain(\"Events\")");
+ gROOT->ProcessLine("chain_2016->Add(\"../samples/WJetsToLNu_HT-200To400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_2016.root\")");
+ gROOT->ProcessLine("prod2016MC_reducedNANO_MET MET_2016(chain_2016)");
  gROOT->ProcessLine(("MET_2016.SetOutput(\""+outFile+"\")").c_str());
  gROOT->ProcessLine("MET_2016.GetEntry(0)");
  gROOT->ProcessLine("MET_2016.Loop()");
- 
+  
  //2017
- gROOT->ProcessLine("TChain chain_2017(\"Events\")");
- gROOT->ProcessLine("chain_2017.Add(\"~/../janguian/ReducedNtuple/output/prod2017MC_reducedNANO_MET.root\")");
- //gROOT->ProcessLine("chain_2017.Add(\"../samples/prod2017MC_reducedNANO_MET.root\")");
- gROOT->ProcessLine("prod2017MC_reducedNANO_MET MET_2017(chain_2017.GetTree())");
+ gROOT->ProcessLine("TChain* chain_2017 = new TChain(\"Events\")");
+ gROOT->ProcessLine("chain_2017->Add(\"../samples/WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8_2017.root\")");
+ gROOT->ProcessLine("prod2017MC_reducedNANO_MET MET_2017(chain_2017)");
  gROOT->ProcessLine(("MET_2017.SetOutput(\""+outFile+"\")").c_str());
  gROOT->ProcessLine("MET_2017.GetEntry(0)");
  gROOT->ProcessLine("MET_2017.Loop()");
 
  //2018
- gROOT->ProcessLine("TChain chain_2018(\"Events\")");
- gROOT->ProcessLine("chain_2018.Add(\"~/../janguian/ReducedNtuple/output/prod2018MC_reducedNANO_MET.root\")");
- //gROOT->ProcessLine("chain_2018.Add(\"../samples/prod2018MC_reducedNANO_MET.root\")");
- gROOT->ProcessLine("prod2018MC_reducedNANO_MET MET_2018(chain_2018.GetTree())");
+ gROOT->ProcessLine("TChain* chain_2018 = new TChain(\"Events\")");
+ gROOT->ProcessLine("chain_2018->Add(\"../samples/WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8_2018.root\")");
+ gROOT->ProcessLine("prod2018MC_reducedNANO_MET MET_2018(chain_2018)");
  gROOT->ProcessLine(("MET_2018.SetOutput(\""+outFile+"\")").c_str());
  gROOT->ProcessLine("MET_2018.GetEntry(0)");
  gROOT->ProcessLine("MET_2018.Loop()");
