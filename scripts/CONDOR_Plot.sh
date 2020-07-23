6fff3e90e77f0924cce9d05d16b20e4785e8c150
Zachary Flowers <zacharyflowers96@gmail.com>
2020-07-23 07:24:28 -0500

    Pre-Split for Condor

--- scripts/CONDOR_Plot.sh
+++ scripts/CONDOR_Plot.sh
@@ -2,10 +2,12 @@
 
 universe = vanilla
 executable = /home/t3-ku/z374f439/Eff_NANO/ReducedNtuple/macros/run_Eff_Nano.x
+notify_user = z374f439@ku.edu
+notification = Complete
 getenv = True
 priority = 10
 use_x509userproxy = true
-request_memory = 4000
+request_memory = 8000
 output = out_test.log
 error = err_test.log
 log = log_test.log
