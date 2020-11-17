e9386aa5c04fa54ec52ab1f0b7b4f2fcfd3f5492
Zachary Flowers <zflowers@login-el7.uscms.org>
2020-11-17 16:21:57 -0600

    Added queueing to jobs

--- macros/Setup_Hist/setup.py
+++ macros/Setup_Hist/setup.py
@@ -7,6 +7,7 @@ path = "/stash/user/zflowers/CMSSW_10_2_20_UL/src/ReducedNtuple/macros/"
 store = path+"HIST/"
 log = path+"LOG_HIST/"
 shell = path+"Shell_HIST/"
+num = path+"Num_HIST/"
 #input = "/home/t3-ku/crogan/NTUPLES/NANO/NEW_21_09_20/"
 input_path = "/stash/user/zflowers/NTUPLES/Processing/"
 
@@ -14,17 +15,52 @@ print("Writing shell scripts")
 
 list_f = []
 
-def write_sh(Cut,Num,Dir,File,Tag):
+def write_sh(Cut,Dir,File,Tag):
     os.system("mkdir -p "+store+Cut+"/"+Dir+"/"+Tag+"/"+File)
     os.system("mkdir -p "+log+Cut+"/"+Dir+"/"+Tag+"/"+File) 
     os.system("mkdir -p "+shell+Cut+"/"+Dir+"/"+Tag+"/"+File) 
-    f = (shell+Cut+"/"+Dir+"/"+Tag+"/"+File+"/"+File+Num+".sh").replace('\n','')
+    f = (shell+Cut+"/"+Dir+"/"+Tag+"/"+File+"/"+File+".sh").replace('\n','')
     fsrc = open(f,'w')
     fsrc.write('CUT = '+Cut+'\n')
     fsrc.write('DIR = '+Dir+'\n')
     fsrc.write('TAG = '+Tag+'\n')
     fsrc.write('FILENAME = '+File+'\n')
-    fsrc.write('NUM = '+Num+'\n')
+    #fsrc.write('NUM = '+Num+'\n')
+    fsrc.write('universe = vanilla \n')
+    fsrc.write('executable = execute_script.sh \n')
+    fsrc.write('#notify_user = z374f439@ku.edu \n')
+    fsrc.write('#notification = Complete \n')
+    fsrc.write('getenv = True \n')
+    fsrc.write('priority = 10 \n')
+    fsrc.write('use_x509userproxy = true \n')
+    fsrc.write('+ProjectName=\"cms.org.ku\" \n')
+    fsrc.write('+REQUIRED_OS=\"rhel7\" \n')
+    fsrc.write('request_memory = 4000 \n')
+    fsrc.write('output = '+log+'$(CUT)/$(DIR)/$(TAG)/$(FILENAME)/out_$(FILENAME).log \n')
+    fsrc.write('error = '+log+'$(CUT)/$(DIR)/$(TAG)/$(FILENAME)/err_$(FILENAME).log \n')
+    fsrc.write('log = '+log+'$(CUT)/$(DIR)/$(TAG)/$(FILENAME)/log_$(FILENAME).log \n')
+    fsrc.write('Requirements = (Machine != \"red-node000.unl.edu\") \n')
+    fsrc.write('Arguments = \"-cut=$(CUT) -tag=$(TAG) -dir=$(DIR) -filename=$(FILENAME) -num=  --hist\" \n')
+    fsrc.write('transfer_input_files = '+path+'config.tgz \n')
+    fsrc.write('should_transfer_files = YES \n')
+    fsrc.write('when_to_transfer_output = ON_EXIT \n')
+    fsrc.write('transfer_output_files = $(CUT)_$(DIR)_$(TAG)_$(FILENAME).root \n')
+    fsrc.write('transfer_output_remaps = \"$(CUT)_$(DIR)_$(TAG)_$(FILENAME).root = '+store+'$(CUT)/$(DIR)/$(TAG)/$(FILENAME)/$(FILENAME).root\" \n')
+    fsrc.write('queue \n')
+    fsrc.close()
+    return f
+
+def write_sh(Cut,Dir,File,Tag):
+    os.system("mkdir -p "+store+Cut+"/"+Dir+"/"+Tag+"/"+File)
+    os.system("mkdir -p "+log+Cut+"/"+Dir+"/"+Tag+"/"+File) 
+    os.system("mkdir -p "+shell+Cut+"/"+Dir+"/"+Tag+"/"+File) 
+    f = (shell+Cut+"/"+Dir+"/"+Tag+"/"+File+"/"+File+".sh").replace('\n','')
+    fsrc = open(f,'w')
+    fsrc.write('CUT = '+Cut+'\n')
+    fsrc.write('DIR = '+Dir+'\n')
+    fsrc.write('TAG = '+Tag+'\n')
+    fsrc.write('FILENAME = '+File+'\n')
+    fsrc.write('NUM = $(Item)\n')
     fsrc.write('universe = vanilla \n')
     fsrc.write('executable = execute_script.sh \n')
     fsrc.write('#notify_user = z374f439@ku.edu \n')
@@ -45,10 +81,17 @@ def write_sh(Cut,Num,Dir,File,Tag):
     fsrc.write('when_to_transfer_output = ON_EXIT \n')
     fsrc.write('transfer_output_files = $(CUT)_$(DIR)_$(TAG)_$(FILENAME)$(NUM).root \n')
     fsrc.write('transfer_output_remaps = \"$(CUT)_$(DIR)_$(TAG)_$(FILENAME)$(NUM).root = '+store+'$(CUT)/$(DIR)/$(TAG)/$(FILENAME)/$(FILENAME)$(NUM).root\" \n')
-    fsrc.write('queue \n')
+    #fsrc.write('queue \n')
+    fsrc.write('queue from '+(num+Cut+"/"+Dir+"/"+Tag+"/"+File+"/"+File+".txt").replace('\n',''))
     fsrc.close()
     return f
 
+def write_num_file(Cut,Dir,File,Tag,Num):
+    os.system("mkdir -p "+num+Cut+"/"+Dir+"/"+Tag+"/"+File)
+    f = (num+Cut+"/"+Dir+"/"+Tag+"/"+File+"/"+File+".txt").replace('\n','')
+    fsrc = open(f,'a+')
+    fsrc.write(Num+'\n')
+    fsrc.close()
 
 os.system("mkdir -p "+path+"config")
 os.system("cp "+path+"Eff_Nano_Hist.x "+path+"config/")
@@ -69,17 +112,19 @@ with open(path+"Setup_Hist/Hist.txt") as cut_handle:
                             Tag = tag_line.replace('\n','')
                             if "SMS" in Dir:
                                 Num = ''
-                                list_f.append(write_sh(Cut,Num,Dir,File,Tag))
+                                list_f.append(write_sh(Cut,Dir,File,Tag))
                             else:
                                 #for num_line in os.listdir(input_path+Dir+"/NoHadd/"+File+"/"):
                                 for num_line in os.listdir(input_path+Dir+"/"+File+"/"):
                                     Num = num_line.replace(File,'')
                                     Num = Num.replace('.root','')
-                                    list_f.append(write_sh(Cut,Num,Dir,File,Tag))
+                                    write_num_file(Cut,Dir,File,Tag,Num)
+                                    #list_f.append(write_sh(Cut,Num,Dir,File,Tag))
+                            list_f.append(write_sh(Cut,Dir,File,Tag))
 
 print("Submitting Jobs")
 list_f = list(dict.fromkeys(list_f))
-for f in list_f:
-    os.system("condor_submit "+f)
+#for f in list_f:
+#    os.system("condor_submit "+f)
 os.system("rm -rf "+path+"config/")
 print("Finished Submitting Jobs")
