f8733c5a7cfea40a8fed9acbf871bcd520357a66
Zachary Flowers <zflowers@login-el7.uscms.org>
2020-12-07 18:23:46 -0600

    Fix config error

--- macros/execute_script_eff.sh
+++ macros/execute_script_eff.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 wget --no-check-certificate http://stash.osgconnect.net/+zflowers/sandbox-CMSSW_10_2_20_UL-9184d4c.tar.bz2
 
-tar -xzf config.tgz
+tar -xzf config_eff.tgz
 
 export SCRAM_ARCH=slc7_amd64_gcc700
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
-source ./config/cmssw_setup.sh
+source ./config_eff/cmssw_setup.sh
 
 cmssw_setup sandbox-CMSSW_10_2_20_UL-9184d4c.tar.bz2 
 
-./config/Eff_Nano_Hist.x "$@"
+./config_eff/Eff_Nano_Hist.x "$@"
---
+++ macros/execute_script_hist.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+wget --no-check-certificate http://stash.osgconnect.net/+zflowers/sandbox-CMSSW_10_2_20_UL-9184d4c.tar.bz2
+
+tar -xzf config_hist.tgz
+
+export SCRAM_ARCH=slc7_amd64_gcc700
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+source ./config_hist/cmssw_setup.sh
+
+cmssw_setup sandbox-CMSSW_10_2_20_UL-9184d4c.tar.bz2 
+
+./config_hist/Eff_Nano_Hist.x "$@"
