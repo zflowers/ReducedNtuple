c7af82c757460c48687f928737644ab29918dace
Zachary Flowers <zacharyflowers96@gmail.com>
2021-05-20 18:49:43 -0500

    Start To Update HT5 Stacker

--- macros/Stacker.C
+++ macros/Stacker.C
@@ -1356,7 +1356,7 @@ void Stacker(vector<string> inFiles, vector<string> cuts){
  //Get2D_Ratio("Mperp_v_RISR_Hist","Mperp_v_RISR_Hist_HEM","PreSelection","HEM","MET_2018","MET_2018",false);
 
  //HEM $$$
- Get2D_Ratio("dphiCMI_v_PTCM_Hist_preHEM","dphiCMI_v_PTCM_Hist_postHEM","PreSelection","PreSelection","MET_2018","MET_2018",true);
+ //Get2D_Ratio("dphiCMI_v_PTCM_Hist_preHEM","dphiCMI_v_PTCM_Hist_postHEM","PreSelection","PreSelection","MET_2018","MET_2018",true);
  //Get2D_Ratio("dphiCMI_v_PTCM_Hist_preHEM","dphiCMI_v_PTCM_Hist_postHEM","PreSelection","HEM-EventFlag_JetInHEM_Pt20E0","MET_2018","MET_2018",true);
  //Get2D_Ratio("dphiCMI_v_PTCM_Hist_preHEM","dphiCMI_v_PTCM_Hist_postHEM","PreSelection","HEM-EventFlag_JetInHEM_Pt20_JetIDE0","MET_2018","MET_2018",true);
  //Get2D_Ratio("dphiCMI_v_PTCM_Hist_preHEM","dphiCMI_v_PTCM_Hist_postHEM","PreSelection","EventFlag_JetInHEM_Pt20E0","MET_2018","MET_2018",true);
@@ -1369,7 +1369,10 @@ void Stacker(vector<string> inFiles, vector<string> cuts){
  //Get2D_Ratio("dphiCMI_v_PTCM_Hist_preHEM","dphiCMI_v_PTCM_Hist_postHEM","HEM_Veto_E1","HEM_Veto-E1","MET_2018","MET_2018",true);
  //Get2D_Ratio("dphiCMI_v_PTCM_Hist_preHEM","dphiCMI_v_PTCM_Hist_postHEM","HEM_Veto_E0","HEM_Veto-E0","MET_2018","MET_2018",true);
  //New HEM $$$
- Get2D_Ratio("dphiCMI_v_PTCM_Hist_preHEM","dphiCMI_v_PTCM_Hist_postHEM","HEM_Veto","HEM_Veto","MET_2018","MET_2018",true);
+ //Get2D_Ratio("dphiCMI_v_PTCM_Hist_preHEM","dphiCMI_v_PTCM_Hist_postHEM","HEM_Veto","HEM_Veto","MET_2018","MET_2018",true);
+ 
+ //HT5 Plots
+ //Something like this and some ratios?? //Get2D_Plot("dphiCMI_v_PTCM_Hist",directories_2D,inFiles,cuts,trigger);
  
  
  //2D Ratio 2017 Cleaning
